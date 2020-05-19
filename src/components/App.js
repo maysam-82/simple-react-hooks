8e9d8cbbe3d0e7f41b789f55952b9fcb8a393647
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-05-20 02:39:16 +1000

    implement useState in App

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,17 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-export default class App extends Component {
-	state = {
-		taskName: 'posts',
-	};
-	render() {
-		return (
+const App = () => {
+	// `useState` will return two items. The first one is a current value of a piece of state and
+	// the second one is a setter function which will update the value of first item. We can pass an initial value for that piece of state as well.
+	const [taskName, setTaskName] = useState('posts');
+	return (
+		<div>
 			<div>
-				<div>
-					<button onClick={() => this.setState({ taskName: 'posts' })}>
-						Posts
-					</button>
-					<button onClick={() => this.setState({ taskName: 'todos' })}>
-						Todos
-					</button>
-				</div>
-				{this.state.taskName}
+				<button onClick={() => setTaskName('posts')}>Posts</button>
+				<button onClick={() => setTaskName('todos')}>Todos</button>
 			</div>
-		);
-	}
-}
+			{taskName}
+		</div>
+	);
+};
+export default App;
